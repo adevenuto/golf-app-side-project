5060190764d638ec5797cdec595ad160eda0fb01
vagrant <vagrant@homestead>
2019-12-28 04:27:45 +0000

    debugging

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     @include('partials/head')
     <body>
-        <div id="app" class="d-flex flex-column">
+        <div id="app">
             <navigation-component user='{{ Auth::user() }}'></navigation-component>
             <div class="my-4">
                 @yield('content')
